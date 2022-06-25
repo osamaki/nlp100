a3867c08840ffd844709f7fca572f0c6fdb58f9f
osamaki <soresunawati@docomo.ne.jp>
2022-06-26 00:59:00 +0900

    add main function

--- haskell/05_n-gram.hs
+++ haskell/05_n-gram.hs
@@ -2,3 +2,9 @@ import qualified Data.List as L
 
 ngram :: Int -> [a] -> [[a]]
 ngram n = filter ((>= n) . length) . map (take n) . L.tails
+
+main = do
+    n <- readLn :: IO Int
+    s <- getLine
+    print $ "Character n-gram: " ++ (show $ ngram n s)
+    print $ "Word n-gram: " ++ (show . ngram n $ words s)
